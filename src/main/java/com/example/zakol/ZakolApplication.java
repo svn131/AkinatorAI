9b998cd07818544992a098becc9c0edca3d6c118
Vladimir Solovyov <svn131131@gmail.com>
2023-10-19 07:55:58 +0500

    Начало. бд с докера заработало- парсит без лишнего

--- src/main/java/com/example/zakol/ZakolApplication.java
+++ src/main/java/com/example/zakol/ZakolApplication.java
@@ -1,9 +1,6 @@
 package com.example.zakol;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ZakolApplication {
@@ -27,49 +26,66 @@ public class ZakolApplication {
     private void run() throws Exception {
 
 
-            FileInputStream file = new FileInputStream("C:/AkinatorAI.xlsx");
+        FileInputStream file = new FileInputStream("C:/AkinatorAI.xlsx");
 
-            Workbook workbook = new XSSFWorkbook(file);
-            Sheet sheet = workbook.getSheetAt(0); // Получение первого листа
+        Workbook workbook = new XSSFWorkbook(file);
+        Sheet sheet = workbook.getSheetAt(0); // Получение первого листа
 
-            // Получение количества ячеек в первой строке
-            Row headerRow = sheet.getRow(0);
-            int numCells = headerRow.getLastCellNum();
+        // Получение количества ячеек в первой строке
+        Row headerRow = sheet.getRow(0);
+        int numCells = headerRow.getLastCellNum();
+        List<String> firstRowValues = new ArrayList<>();
 
-            // Парсинг значений первой строки в массив
-            String[] firstRowValues = new String[numCells];
-            for (int i = 0; i < numCells; i++) {
-                Cell cell = headerRow.getCell(i);
-                firstRowValues[i] = cell.getStringCellValue();
+        for (int i = 0; i < numCells; i++) {
+            Cell cell = headerRow.getCell(i);
+
+            if (cell != null && cell.getCellType() == CellType.STRING) {
+                String cellValue = cell.getStringCellValue();
+                firstRowValues.add(cellValue);
             }
+        }
 
-            // Парсинг значений первого столбца в массив
-            int numRows = sheet.getLastRowNum() + 1;
-            String[] firstColumnValues = new String[numRows];
-            for (int i = 0; i < numRows; i++) {
-                Row row = sheet.getRow(i);
-                Cell cell = row.getCell(0);
-                firstColumnValues[i] = cell.getStringCellValue();
+// Преобразование ArrayList в массив
+        String[] firstRowArray = firstRowValues.toArray(new String[0]);
+
+
+        // Парсинг значений первого столбца в массив
+        int numRows = sheet.getLastRowNum() + 1;
+        List<String> firstColumnValues = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            Row row = sheet.getRow(i);
+            Cell cell = row.getCell(0);
+
+            if (cell != null && cell.getCellType() == CellType.STRING) {
+                String cellValue = cell.getStringCellValue().trim();
+                if (!cellValue.isEmpty()) {
+                    firstColumnValues.add(cellValue);
+                }
             }
+        }
 
-            // Вывод значений массивов
-            System.out.println("Значения первой строки:");
-            for (String value : firstRowValues) {
-                System.out.println(value);
-            }
+// Преобразование ArrayList в массив
+        String[] firstColumnArray = firstColumnValues.toArray(new String[0]);
 
-            System.out.println("Значения первого столбца:");
-            for (String value : firstColumnValues) {
-                System.out.println(value);
-            }
 
-            workbook.close();
-            file.close();
+        // Вывод значений массивов
+        System.out.println("Значения первой строки:");
+        for (String value : firstRowValues) {
+            System.out.println(value);
+        }
+
+        System.out.println("Значения первого столбца:");
+        for (String value : firstColumnValues) {
+            System.out.println(value);
+        }
+
+        workbook.close();
+        file.close();
 
     }
 
 
-
-    }
+}
 
 
